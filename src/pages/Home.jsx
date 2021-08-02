45ec2405725104f1dc5f1aa678421e98cdaab745
GustavoGJesus <icarogabriel01@protonmail.com>
2021-08-02 12:38:43 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Styles from '../styles/home.scss'
+
+import Profile from '../assets/Perfil.jpg'
+
+function Home (){
+    return(
+        <div id="page-home">
+          <div id="profile"/>
+          
+          <h1>Gustavo Gomes</h1>
+
+
+        </div>
+    )
+}
+
+export default Home;

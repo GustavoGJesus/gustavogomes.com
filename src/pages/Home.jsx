478e882f0817a2f921edd9ba6682cf989b66e0d5
GustavoGJesus <icarogabriel01@protonmail.com>
2021-08-02 22:36:35 -0300

    Typing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Styles from '../styles/home.scss'
 
-import Profile from '../assets/Perfil.jpg'
-
+import Typing from 'react-typing-animation'
 function Home (){
-    return(
+    
+  
+  return(
         <div id="page-home">
           <div id="profile"/>
-          
           <h1>Gustavo Gomes</h1>
-
-
+          <Typing>
+            <h2>Frontend Engineer React</h2>
+            <Typing.Delay ms={1000} />
+           </Typing>
         </div>
     )
 }
-
 export default Home;

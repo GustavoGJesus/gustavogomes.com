14b4aa8cc5ae2af045929a5e985155eb17ea346d
GustavoGJesus <icarogabriel01@protonmail.com>
2021-08-04 12:47:43 -0300

    Finish Page Home

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import Typing from 'react-typing-animation'
 
-import Styles from '../styles/home.scss'
+import Styles from '../styles/profile.scss'
 
 export default function Profile(){
     return(
@@ -12,10 +12,23 @@ export default function Profile(){
                 <h2>Frontend Engineer</h2>
                 <Typing.Delay ms={1000} />
             </Typing>
+
+            <div className="separator">//</div>
+            
             <div className="about">
+                <h1 className="about-h1">
+                    Hey!
+                </h1>
                 <p>
-                Hey, I'am Software Engineer with specialty in JavaScript / NodeJs / React
+                    I'am Software Engineer with specialty in JavaScript / ReactJs / NodeJs .
+                    <br />
+                    I am also a Computer Science student at Centro Universitário de Brasília.
+                    <br />
+                    I am an admirer of Technology and Innovation, I am always looking for new knowledge so that I can achieve results with them.
+                    <br />
+                    My goal is to make people's lives easier through Technology.
                 </p>
+                <div className="separator">//</div>
             </div>
         </div>
     );

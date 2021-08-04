4cda4535de32689ebf505668a6ea6424b9a37ddf
GustavoGJesus <icarogabriel01@protonmail.com>
2021-08-04 01:05:49 -0300

    creating const

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,29 @@ import React, { useEffect, useState } from 'react';
 import Styles from '../styles/home.scss'
 
 import Typing from 'react-typing-animation'
-function Home (){
-    
+export default function Home (){
+    const title = "Gustavo Gomes";
+    const name = "Gustavo Gomes"
+    const descriptionOne = "Frontend Engineer";
   
-  return(
-        <div id="page-home">
-          <div id="profile"/>
-          <h1>Gustavo Gomes</h1>
-          <Typing>
-            <h2>Frontend Engineer React</h2>
-            <Typing.Delay ms={1000} />
-           </Typing>
-        </div>
+return(
+  <div id="page-home">
+    <div id="profile"/>
+    <title>{title}</title>
+    <h1>{name}</h1>
+    <Typing>
+      <h2>{descriptionOne}</h2>
+      <Typing.Delay ms={1000} />
+    </Typing>
+        
+    <div id="about">
+      <p>
+         Hey, I'am Software Engineer with specialty in JavaScript / NodeJs / React
+      </p>
+    </div>
+
+
+  </div>
+      
     )
 }
-export default Home;

a93e167bae631d51d19039b9f243cb354a868b30
GustavoGJesus <icarogabriel01@protonmail.com>
2021-08-16 01:21:47 -0300

    Cards width and margin

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -28,6 +28,7 @@ export default function CardMotion(){
 
     return(
     <>
+    <h1>Projects</h1>
     <body className="body">
         <div className="container">
             <div className="carousel" ref={carousel}>
